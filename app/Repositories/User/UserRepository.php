23084896178fb536a3099cc54359cf55b4ba30d6
Jerson Baltazar <work.jerson.baltazar@gmail.com>
2019-11-02 12:01:54 +0800

    Initialized User Repository

---
+++ app/Repositories/User/UserRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Articles\Repositories;
+
+use App\User;
+use App\Repositories\UserRepositoryInterface;
+use App\Base\BaseRepository;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+    protected $model;
+
+    /**
+     * ArticlesRepository constructor.
+     * @param Article $article
+     */
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+}
---
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\Base\BaseRepositoryInterface;
+
+interface UserRepositoryInterface extends BaseRepositoryInterface
+{
+    //
+}

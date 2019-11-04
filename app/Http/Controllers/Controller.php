b26bee2df2049e043734d0b183613d9c02446ad9
Jerson Baltazar <jerson.baltazar@myxlog.com>
2019-11-04 14:55:59 +0800

    Added code in controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,79 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Generate cached response if applicable.
+     *
+     * @param \Closure $callback
+     *
+     * @return mixed
+     */
+    protected function generateCachedResponse($callback)
+    {
+        // TODO: For refactor.
+        return $callback();
+
+        $params = request()->query();
+        ksort($params);
+        $rememberKey = sha1(request()->url().http_build_query($params));
+    }
+
+    /**
+     * Paginate the dataset if applicable.
+     *
+     * @param Builder $builder
+     *
+     * @return Collection
+     */
+    protected function paginateOrGet($builder)
+    {
+        if (request()->has('page')) {
+            return $builder->paginate(request('per_page'));
+        }
+
+        if ($builder instanceof \Illuminate\Support\Collection) {
+            return $builder->values();
+        }
+
+        return $builder->get();
+    }
+
+    /**
+     * Switch to CMS or WEB equivalent function.
+     *
+     * @param string $method
+     * @param mixed  $request
+     * @param mixed  $model
+     *
+     * @return Function
+     */
+    protected function switch(string $method, $request = null, $model = null)
+    {
+        if (request()->user()->is_cms ?? false) {
+            $method .= 'Cms';
+        } else {
+            $method .= 'Web';
+        }
+
+        return $this->$method($request, $model);
+    }
+
+    /**
+     * Get the map of resource methods to ability names.
+     *
+     * @return array
+     */
+    protected function resourceAbilityMap()
+    {
+        return [
+            'index' => 'view',
+            'show' => 'view',
+            'create' => 'create',
+            'store' => 'create',
+            'edit' => 'update',
+            'update' => 'update',
+            'destroy' => 'delete',
+        ];
+    }
 }

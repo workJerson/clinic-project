03decab7aacb79057dcb7ae088a74b6eeb33cd45
Jerson Baltazar <work.jerson.baltazar@gmail.com>
2019-11-08 20:36:44 +0800

    Added traits and methods for passport authentication

--- app/User.php
+++ app/User.php
@@ -2,13 +2,22 @@
 
 namespace App;
 
+use App\Traits\Filterable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Filterable, HasApiTokens;
+
+    /**
+     * User Account Types
+     * 1 = Super Admin
+     * 2 = CMS User
+     * 3 = Patient
+     */
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +45,50 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function findForPassport($email)
+    {
+        return self::where('email', $email)->first();
+    }
+
+    /**
+     * Increment the login attempts of the user.
+     */
+    public function incrementLoginAttempts()
+    {
+        $this->increment('login_attempts');
+
+        if ($this->login_attempts >= 3) {
+            $this->deactivate();
+        }
+    }
+
+    /**
+     * Clear the user's number of login attempts.
+     */
+    public function clearLoginAttempts()
+    {
+        $this->login_attempts = 0;
+        $this->save();
+    }
+
+    /**
+     * Deactivate the user.
+     */
+    public function deactivate()
+    {
+        $this->status = 0;
+
+        $this->save();
+    }
+
+    public function isSuperAdmin()
+    {
+        return $this->is_cms && $this->account_type == 1;
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }

346af10d6564978f41acbabe2c14ad3d00fd54f1
Jerson Baltazar <jerson.baltazar@myxlog.com>
2019-11-25 18:00:59 +0800

    Initialized Patient & Service Controller

--- public/js/app.js
+++ public/js/app.js
@@ -34567,8 +34567,8 @@ function view(name) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Administrator\Desktop\clinic-project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Administrator\Desktop\clinic-project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/xloguser/Desktop/Jerson/clinic-project/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/xloguser/Desktop/Jerson/clinic-project/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
